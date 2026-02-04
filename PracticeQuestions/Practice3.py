177f501e331fa252bf7ad7b12cbb12719b4d745d
Aman Tiwari <askaman615@gmail.com>
2026-02-05 01:12:01 +0530

    DAY05

--- PracticeQuestions/Practice3.py
+++ PracticeQuestions/Practice3.py
@@ -4,13 +4,23 @@
 nums = [55, 32, -97, 99, 3, 67]
        
 maxi = float('-inf')
-second_max = 0
+second_max = float("-inf")
 
+# for i in range(0, len(nums)):
+#     if nums[i] > maxi:
+#         maxi = nums[i]
+    
+# for i in range(0, len(nums)):
+#     if nums[i] > second_max and nums[i] != maxi:
+        # second_max = nums[i]
+        
 for i in range(0, len(nums)):
     if nums[i] > maxi:
         second_max = maxi
-        maxi = nums[i]
-    
+        maxi = nums[i] 
+    elif nums[i] > second_max and nums[i] != maxi:
+        second_max = nums[i]
+        
 print(maxi)
 print(second_max)
 
